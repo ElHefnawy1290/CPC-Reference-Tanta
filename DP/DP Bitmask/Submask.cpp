3ee3295ada18c15bd66cbad1c446189ecdd48f1f
ElHefnawy1290 <amr223489@gmail.com>
2026-08-28 17:11:36 +0300

    Update Submask.cpp

--- DP/DP Bitmask/Submask.cpp
+++ DP/DP Bitmask/Submask.cpp
@@ -1,17 +1,35 @@
-ll dp[1 << 16], score[1 << 16];
-
-ll sol(ll mask)
-{
-    if (mask == 0)
-        return 0;
+int dp[1<<16];
+int cost[1<<16];
+int memo (int mask) {
+    if (mask == 0) return 0;
     auto &ret = dp[mask];
-    if (~ret)
-        return ret;
-    ret = -OO;
-    for (ll sub = mask; sub; sub = (sub - 1) & mask)
-    {
-        ll sum = score[sub];
-        ret = max(ret, sum + sol(mask ^ sub));
+    if (~ret) return ret;
+    for (int i = mask;i > 0;i = (i-1) & mask) {
+        ret = max(ret,cost[i] + memo(mask ^ i));
     }
     return ret;
+}
+void hassan() {
+    int n;cin >>n;
+    int a[n][n];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> a[i][j];
+        }
+    }
+    for (int mask = 0; mask < (1<<n); mask++) {
+        for (int i = 0; i < n; i++) {
+            if (mask & (1<<i)) {
+                for (int j = 0; j < i; j++) {
+                    if (mask & (1<<j)) {
+                        cost[mask]+=a[i][j];
+                    }
+                }
+            }
+        }
+      //  cout << mask << " >> " << cost[mask] << endl;
+    }
+    memset(dp, -1, sizeof(dp));
+    cout << memo((1<<n)-1);
+
 }
