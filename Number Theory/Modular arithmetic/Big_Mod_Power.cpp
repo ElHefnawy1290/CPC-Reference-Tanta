ab60882db83fbd1822c18daee87ba6b2d9bef6b0
ElHefnawy1290 <amr223489@gmail.com>
2026-08-28 17:11:39 +0300

    Create Big_Mod_Power.cpp

---
+++ Number Theory/Modular arithmetic/Big_Mod_Power.cpp
@@ -0,0 +1,11 @@
+/// if we need to calculate (n % m) if  EX: n = 10^10000 and m = 10^9
+ll Big_Mod_Power(string n, ll m) 
+{
+    ll x = 0;
+    for(ll i = 0; i < n.size(); i++)
+    {
+        x = ((x * 10)) + (n[i] - '0');
+        x %= m;  // كل مره بناخد باقي قسمه عشان نقلل الرقم
+    }
+    return x;  /// return n % m
+}
