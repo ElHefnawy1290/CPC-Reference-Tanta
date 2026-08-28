689a4a570da77594eda4a7e10a40c818ce612dd8
ElHefnawy1290 <amr223489@gmail.com>
2026-08-28 17:11:39 +0300

    Update Matrix_expo.cpp

--- Math/Matrix/Matrix_expo.cpp
+++ Math/Matrix/Matrix_expo.cpp
@@ -45,3 +45,31 @@ Matrix I(int n)
         T[i][i] = 1;
     return T;
 }
+
+// 2d Grid represent as graph
+// count paths
+short dx[8] = {1,1,-1,-1,2,2,-2,-2};
+short dy[8] = {2,-2,2,-2,1,-1,1,-1};
+void solve() {
+    int k;cin >>k;
+    Matrix T(65,Row(65));
+    for (int i = 0;i<8;i++) {
+        for (int j = 0;j<8;j++) {
+            int cur = i*8 + j;
+            for (int k = 0;k<8;k++) {
+                short ni = i+dx[k];
+                short nj = j+dy[k];
+                if (ni < 0 || nj < 0 || ni > 7 || nj>7) continue;
+                int nCur = ni*8+nj;
+                T[nCur][cur] = 1;
+            }
+        }
+    }
+    for (int i = 0;i<65;i++) {
+        T[i][64] = 1;
+    }
+    T = fastPower(T,k+1);
+    int ans = T[0][64];
+    cout << ans;
+}
+ 
