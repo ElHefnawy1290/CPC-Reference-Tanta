4985bae95b2a5891dbd8d98d0fe3380d081152a3
ElHefnawy1290 <amr223489@gmail.com>
2026-08-28 17:11:43 +0300

    Update Ordered_set.cpp

--- Helpers/Ordered_set.cpp
+++ Helpers/Ordered_set.cpp
@@ -1,3 +1,7 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
 typedef tree<int, null_type, less<>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 
 void myerase(ordered_set &t, int v)
@@ -6,3 +10,8 @@ void myerase(ordered_set &t, int v)
     auto it = t.find_by_order(rank);
     t.erase(it);
 }
+/*
+ *  t.order_of_key(v);           // Returns the NUMBER of elements strictly less than v (the rank)
+ *  *t.find_by_order(k);         // Returns the value at index k (0-indexed)
+ *  myerase(t, v);               // Safely erases exactly ONE instance of v
+*/
